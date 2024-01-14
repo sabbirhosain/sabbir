483f6692af888e73c607ac11dfddf865bd557ce7
Sabbir <sabbirhosainbd59@gmail.com>
2024-01-14 12:22:39 +0600

    service card design

--- src/Components/Service/ServiceCard.jsx
+++ src/Components/Service/ServiceCard.jsx
@@ -9,8 +9,8 @@ const ServiceCard = () => {
           <div class="service-icon">
             <i><FaFileCode /></i>
           </div>
-          <h4 className='service__title'>Web Application</h4>
-          <p className='service__text'>Lorem Ipsum is simply dummy text of industry. Lorem Ipsum has been the industry...</p>
+          <h4 className='service__title'>Website Development</h4>
+          <p className='service__text'>Creating visually appealing and functional websites tailored to the client's needs...</p>
           <Link className='service__btn'>Read More</Link>
         </div>
       </div>
@@ -19,8 +19,8 @@ const ServiceCard = () => {
           <div class="service-icon">
             <i><FaFileCode /></i>
           </div>
-          <h4 className='service__title'>Web Application</h4>
-          <p className='service__text'>Lorem Ipsum is simply dummy text of industry. Lorem Ipsum has been the industry...</p>
+          <h4 className='service__title'>Responsive Web Design</h4>
+          <p className='service__text'>Ensuring that websites work seamlessly across various devices and screen sizes...</p>
           <Link className='service__btn'>Read More</Link>
         </div>
       </div>
@@ -29,8 +29,8 @@ const ServiceCard = () => {
           <div class="service-icon">
             <i><FaFileCode /></i>
           </div>
-          <h4 className='service__title'>Web Application</h4>
-          <p className='service__text'>Lorem Ipsum is simply dummy text of industry. Lorem Ipsum has been the industry...</p>
+          <h4 className='service__title'>React Development</h4>
+          <p className='service__text'>Fully Functional and user friendly responsive website developments...</p>
           <Link className='service__btn'>Read More</Link>
         </div>
       </div>
@@ -39,8 +39,8 @@ const ServiceCard = () => {
           <div class="service-icon">
             <i><FaFileCode /></i>
           </div>
-          <h4 className='service__title'>Web Application</h4>
-          <p className='service__text'>Lorem Ipsum is simply dummy text of industry. Lorem Ipsum has been the industry...</p>
+          <h4 className='service__title'>Front-End Development</h4>
+          <p className='service__text'>Building the user interface and user experience using HTML, CSS, and JavaScript...</p>
           <Link className='service__btn'>Read More</Link>
         </div>
       </div>
@@ -49,8 +49,8 @@ const ServiceCard = () => {
           <div class="service-icon">
             <i><FaFileCode /></i>
           </div>
-          <h4 className='service__title'>Web Application</h4>
-          <p className='service__text'>Lorem Ipsum is simply dummy text of industry. Lorem Ipsum has been the industry...</p>
+          <h4 className='service__title'>Back-End Development</h4>
+          <p className='service__text'>Developing the server-side of the website, handling database interactions, and managing server logic...</p>
           <Link className='service__btn'>Read More</Link>
         </div>
       </div>
@@ -59,8 +59,8 @@ const ServiceCard = () => {
           <div class="service-icon">
             <i><FaFileCode /></i>
           </div>
-          <h4 className='service__title'>Web Application</h4>
-          <p className='service__text'>Lorem Ipsum is simply dummy text of industry. Lorem Ipsum has been the industry...</p>
+          <h4 className='service__title'>E-commerce Website</h4>
+          <p className='service__text'>Building online stores, integrating payment gateways, and ensuring secure online transactions...</p>
           <Link className='service__btn'>Read More</Link>
         </div>
       </div>
@@ -69,8 +69,8 @@ const ServiceCard = () => {
           <div class="service-icon">
             <i><FaFileCode /></i>
           </div>
-          <h4 className='service__title'>Web Application</h4>
-          <p className='service__text'>Lorem Ipsum is simply dummy text of industry. Lorem Ipsum has been the industry...</p>
+          <h4 className='service__title'>(SEO) Optimization</h4>
+          <p className='service__text'>Optimizing websites to improve their visibility on search engines and drive organic traffic...</p>
           <Link className='service__btn'>Read More</Link>
         </div>
       </div>
@@ -79,8 +79,8 @@ const ServiceCard = () => {
           <div class="service-icon">
             <i><FaFileCode /></i>
           </div>
-          <h4 className='service__title'>Web Application</h4>
-          <p className='service__text'>Lorem Ipsum is simply dummy text of industry. Lorem Ipsum has been the industry...</p>
+          <h4 className='service__title'>Wordpress Website Design</h4>
+          <p className='service__text'>Implementing CMS platforms like WordPress for easy content management...</p>
           <Link className='service__btn'>Read More</Link>
         </div>
       </div>
